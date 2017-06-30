8ad987fff4710a1d6360da24e80238c44daf1dc0
Kassandra Meyer <kassandra@whereby.us>
2017-06-30 13:58:39 -0400

    setup icons, utils, components, css, and App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 
-
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <div>Hello World</div>
+       </div>
+    );
+  }
+}
 
 export default App;
