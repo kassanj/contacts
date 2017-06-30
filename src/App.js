04d50634a6a2b50816db35f40f8b73c2c275451b
Kassandra Meyer <kassandra@whereby.us>
2017-06-30 13:48:59 -0400

    remove files and remove code

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
 import React, { Component } from 'react';
 
 
-class ContactList extends React.Component {
-  render() {
 
-    // const people = [
-    //   { name: 'Michael' },
-    //   { name: 'Kassandra' },
-    //   { name: 'Ernie' }
-    // ]
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map(person => (
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: 'Michael' },
-          { name: 'Kassandra' },
-          { name: 'Ernie' }
-        ]}/>
-          <ContactList contacts={[
-          { name: 'John' },
-          { name: 'Sarah' },
-          { name: 'Tyler' }
-        ]}/>
-      </div>
-    );
-  }
-}
 
 export default App;
