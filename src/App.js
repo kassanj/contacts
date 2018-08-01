ed275efcb44d7e37f27f2acef5fcf0e0903d2b6f
kassanj <kassandra@whereby.us>
2018-08-01 13:39:42 -0700

    fix: createContact bug

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react'
 import ListContacts from './ListContacts'
-import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
     contacts: []
   }
   componentDidMount() {
-    ContactsAPI.getAll().then((contacts) => {
-      this.setState({ contacts })
-    })
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
   }
   removeContact = (contact) => {
-    this.setState((state) => ({
-      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
     }))
 
     ContactsAPI.remove(contact)
   }
-  createContact(contact) {
-    ContactsAPI.create(contact).then(contact => {
-      this.setState(state => ({
-        contacts: state.contacts.concat([ contact ])
-      }))
-    })
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
   }
   render() {
     return (
       <div>
         <Route exact path='/' render={() => (
           <ListContacts
-            onDeleteContact={this.removeContact}
             contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
           />
-        )}/>
+        )} />
         <Route path='/create' render={({ history }) => (
           <CreateContact
             onCreateContact={(contact) => {
@@ -43,12 +49,10 @@ class App extends Component {
               history.push('/')
             }}
           />
-        )}/>
+        )} />
       </div>
     )
   }
 }
 
-export default App;
-
-
+export default App
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -24,7 +24,7 @@ class CreateContact extends Component {
           />
           <div className='create-contact-details'>
             <input type='text' name='name' placeholder='Name'/>
-            <input type='text' name='email' placeholder='Email'/>
+            <input type='text' name='handle' placeholder='Handle'/>
             <button>Add Contact</button>
           </div>
         </form>
